1aabe8e771b57be19cab47525a178ee6cdff5c4b
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-14 13:43:59 +0530

    video(folder 5 th outputting conditional content)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,12 +7,37 @@ import ExpenseFilter from "./ExpenseFilter";
 
 function Expenses(props)
 {
-   const [filteredYear,setfilteredYear]=useState('');
+   const [filteredYear,setfilteredYear]=useState('2020');
     const filterChangeHandler=selectedYear=>
 {
     setfilteredYear(selectedYear);
 }
 
+const filteredExpenses=props.items.filter(expense=>{
+
+    return expense.date.getFullYear().toString()===filteredYear;
+});
+
+
+let expensesContent=<p style={{color:'white'}}>No Expenses Found</p>
+
+if(filteredExpenses.length>0)
+{
+
+    expensesContent=filteredExpenses.map((expense)=>(
+    
+    <ExpenseItem
+
+       key={expense.id}
+       title={expense.title} 
+       amount={expense.amount} 
+       date={expense.date}
+       
+       />
+        
+    
+    ));
+};
 
 return(
 <Card className="expenses">
@@ -20,20 +45,7 @@ return(
 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
 
        
-{props.items.map((expense=>
-
-     
-       <ExpenseItem
-
-      key={expense.id}
-      title={expense.title} 
-      amount={expense.amount} 
-      date={expense.date}
-      
-      />
-
-))
-}
+{expensesContent}
     </Card>
 );
 }
